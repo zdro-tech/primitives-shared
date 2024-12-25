aaa4af1157a5a9b538d476eb95951ad51f53b02d
Vadim <vadim.sanko@gmail.com>
2024-12-25 18:04:06 +0300

    Add accessCode to ConfirmedPrescription

--- src/types/doctor.ts
+++ src/types/doctor.ts
@@ -37,6 +37,7 @@ export interface ConfirmedPrescription extends Prescription {
     updatedAt?: Date;
     upatedBy: string;
     numberOfPackages: number;
+    accessCode?: string;
 }
 
 export interface ConfirmedPrescriptionWithMeds extends ConfirmedPrescription {
