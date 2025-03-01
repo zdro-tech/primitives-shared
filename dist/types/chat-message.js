5213d7e2b0941236616fa853bba4b7d901ca1f6c
Antoni <antonibertel@gmail.com>
2025-03-01 15:40:15 +0000

    Minor update

--- dist/types/chat-message.js
+++ dist/types/chat-message.js
@@ -18,18 +18,6 @@ export var MessagePropagation;
 export var MessageClass;
 (function (MessageClass) {
     MessageClass["MEDICAL_INFORMATION"] = "MEDICAL_INFORMATION";
-    MessageClass["GREETINGS"] = "GREETINGS";
-    MessageClass["GRATITUDE"] = "GRATITUDE";
-    MessageClass["OFFTOPIC"] = "OFFTOPIC";
-    MessageClass["OFFTOPIC_WARNING"] = "OFFTOPIC_WARNING";
-    MessageClass["CONTAINS_QUESTIONS"] = "CONTAINS_QUESTIONS";
-    MessageClass["CONTAINS_QUESTIONS_NOTIFICATION"] = "CONTAINS_QUESTIONS_NOTIFICATION";
-    MessageClass["TOO_SHORT_TEXT"] = "TOO_SHORT_TEXT";
-    MessageClass["TOO_SHORT_TEXT_WARNING"] = "TOO_SHORT_TEXT_WARNING";
-    MessageClass["CHILD_RELATED"] = "CHILD_RELATED";
-    MessageClass["CHILD_RELATED_WARNING"] = "CHILD_RELATED_WARNING";
-    MessageClass["LIFE_THREATENING"] = "LIFE_THREATENING";
-    MessageClass["LIFE_THREATENING_WARNING"] = "LIFE_THREATENING_WARNING";
     MessageClass["SYSTEM_REPLY"] = "SYSTEM_REPLY";
     MessageClass["CHAT_REPLY"] = "CHAT_REPLY";
     MessageClass["UNIDENTIFIED"] = "UNIDENTIFIED";
